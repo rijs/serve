fbac69430f8224bdfa08a1581ec6112baf3df4ff
pemrouz <p3pedram@hotmail.com>
2018-03-25 22:57:41 +0100

    feat: remove need to bundle client files

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ module.exports = function serve(ripple, { server, serve = __dirname, client = 'r
 
   app.use(`/${client}.js`, compress, send(path('js')))
   app.use(`/${client}.min.js`, compress, send(path('min.js')))
-  app.use(`/${client}.pure.js`, compress, send(path('pure.js')))
-  app.use(`/${client}.pure.min.js`, compress, send(path('pure.min.js')))
   return ripple
 }
 
@@ -20,7 +18,7 @@ const expressify = server => server.express
   || key('_events.request')(server) 
   || server.on('request', express())._events.request
 
-const local = (path, client) => ext => resolve(path, `./dist/${client}.${ext}`)
+const local = (path, client) => ext => resolve(path, `./${client}.${ext}`)
 
 const compression = require('compression')
     , send = require('utilise/send')
